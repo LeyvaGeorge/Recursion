f41c62ced8acc7176f0b16941a022b5847db7a3d
George Leyva <leyvageorge@hotmail.com>
2026-04-16 21:22:13 -0700

    adding GUI

--- Recursion.java
+++ Recursion.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+import java.awt.*;
+import javax.swing.*;
+
 class Recursion {
     public int recursive(int iterations, Scanner scnr) {
         if (iterations == 0) {  //Base case: if iterations is 0
@@ -13,16 +16,82 @@ class Recursion {
     }
 
     public static void main(String[] args) {
+        int iterations = 5;
+        int results = 0; 
+        JFrame frame = null; //Frame for the GUI
+        JLabel label = null;    //Label to display the prompt for user input
+        JLabel resultLabel = null; //Label to display the result of the recursion
+        JTextField numbRecursion = null; //Text field for user to enter the number of recursions
+        JTextField resultField = null; //Text field to display the result of the recursion
+        GridBagConstraints layoutConst = null; //Layout constraints for the GUI components
+
+        //Set Labels
+        label = new JLabel("Number of Recursions:");
+        resultLabel = new JLabel("Result: ");
+
+        //Set Text Fields
+        numbRecursion = new JTextField(10);
+        numbRecursion.setEditable(false);
+        numbRecursion.setText(Integer.toString(iterations));
+        
+        resultField = new JTextField(10);
+        resultField.setEditable(false);
+        resultField.setText(Integer.toString(results));
+        
+        //Creating frame
+        frame = new JFrame("Recursion");
+        
+        //Using a GridBagLayout
+        frame.setLayout(new GridBagLayout());
+
+        //Creating GridBagConstraints
+        layoutConst = new GridBagConstraints();
+
+        //Specify component's grid location
+        layoutConst.gridx = 0;
+        layoutConst.gridy = 0;
+
+        // 10 pixels of padding around component
+        layoutConst.insets = new Insets(10, 10, 10, 10);
+
+        //Add component using the specified constraints
+        frame.add(label, layoutConst);  //first label on top left
+
+        layoutConst = new GridBagConstraints(); //Second Text field on top right
+        layoutConst.gridx = 1;
+        layoutConst.gridy = 0;
+        layoutConst.insets = new Insets(10, 10, 10, 10);
+        frame.add(numbRecursion, layoutConst);
+
+        layoutConst = new GridBagConstraints(); //Third label on bottom left
+        layoutConst.gridx = 0;
+        layoutConst.gridy = 1;
+        layoutConst.insets = new Insets(10, 10, 10, 10);
+        frame.add(resultLabel, layoutConst);
+
+        layoutConst = new GridBagConstraints(); //Fourth Text field on bottom right
+        layoutConst.gridx = 1;
+        layoutConst.gridy = 1;
+        layoutConst.insets = new Insets(10, 10, 10, 10);
+        frame.add(resultField, layoutConst);
+
+
+        //Terminate program whe window closes
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Resize the frame to fit the components
+        frame.pack();
+
+        //Display window
+        frame.setVisible(true);
         //Create an instance of the Recursion class
         Recursion rec = new Recursion();
         //Create a Scanner object to read user input
         Scanner input = new Scanner(System.in);
 
-        int iterations = 5; 
         int result = rec.recursive(iterations, input);  //Recursion is used to find the product
         System.out.println("Product: " + result);   
 
         input.close();
     }
-
 }
